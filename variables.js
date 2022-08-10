40174d09ca3c05f3be5775bb20ff1244c2b5082a
Jhonatan Sánchez <jhonatanjes@gmail.com>
2022-08-10 15:14:23 -0600

    Empezando curso practico JS - Variables

---
+++ variables.js
@@ -0,0 +1,13 @@
+//Para crear una variable podemos usar: var, let (reasignable) y const (no reasignarle)
+let nombre ='Jhonatan';
+let apellido = 'Sánchez';
+let username = 'jsanchez';
+let edad = 33;
+let mail = 'jhonatanjes@gmail.com';
+let esMayorDeEdad = true;
+let dineroAhorrado = 1000;
+let deudas = 100;
+
+
+let nombreCompleto = nombre + ' ' + apellido; //Concatenacion
+let dineroReal = dineroAhorrado - deudas; //Operacion
