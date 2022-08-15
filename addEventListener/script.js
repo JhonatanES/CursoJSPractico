aa371b695a2ac85a55d2c4aa6ae643cff36611c5
Jhonatan Sánchez <jhonatanjes@gmail.com>
2022-08-15 15:50:14 -0600

    Ejercicio de Curso practico JS - AddEventListener - Usando el evento submit de un formulario

--- addEventListener/script.js
+++ addEventListener/script.js
@@ -1,12 +1,14 @@
 const h1 = document.querySelector('h1');
+const form = document.querySelector('#form');
 const input1 = document.querySelector('#calculo1');
 const input2 = document.querySelector('#calculo2');
 const btn = document.querySelector('#btnCalcular');
 const pResult = document.querySelector('#result');
 
-btn.addEventListener('click', btnOnClick);
+form.addEventListener('submit', sumarInputValues);
 
-function btnOnClick() {
+function sumarInputValues(event) {
+    event.preventDefault(); //Para no ejecutar por defecto el evento submit
     const sumaInputs = Number(input1.value) + Number(input2.value);
     pResult.innerText = "Resultado: " + sumaInputs;
 }
